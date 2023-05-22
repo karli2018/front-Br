926609b7226d236e8947babc524140c29e3db1ff
karli2018 <karli2018u@gmail.com>
2023-05-21 21:50:35 -0500

    feat(main): add validations

--- src/components/forms/NewAlumno.tsx
+++ src/components/forms/NewAlumno.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from "react";
+import "../../App.css";
 import { Button, Form } from "react-bootstrap";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { AuthContext } from "../../context/AuthContext";
@@ -18,7 +19,8 @@ interface Props {
   update?: boolean;
 }
 export const NewAlumno = ({ update }: Props) => {
-  const { register, handleSubmit, setValue } = useForm<Inputs>();
+  const { register, formState: { errors},
+   handleSubmit, setValue } = useForm<Inputs>();
   const { authState } = useContext(AuthContext);
   const params = useParams();
   const { createAlumno, AlumnoState, updateAlumno } = useAlumnos({
@@ -53,73 +55,117 @@ export const NewAlumno = ({ update }: Props) => {
         <Form.Label>Primer nombre</Form.Label>
         <Form.Control
           type="text"
-          placeholder="ingrese su nombre"
+          placeholder="Ingrese su nombre"
           {...register("fname", {
             required: true,
+            pattern: {
+              value: /^[A-Za-z ]+$/,
+              message: "Solo letras"
+            }
           })}
         />
+        {errors.fname?.type === 'pattern' && <p className="messages">Ingrese solo letras</p>}
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Segundo nombre</Form.Label>
         <Form.Control
           type="text"
-          placeholder="ingrese su segundo nombre"
-          {...register("sname", {})}
+          placeholder="Ingrese su segundo nombre"
+          {...register("sname", {
+            pattern: {
+              value: /^[A-Za-z ]+$/,
+              message: "Solo letras"
+            }
+          })}
         />
+        {errors.sname?.type === 'pattern' && <p className="messages">Ingrese solo letras</p>}
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Primer apellido</Form.Label>
         <Form.Control
           type="text"
-          placeholder="ingrese su primer apellido"
+          placeholder="Ingrese su primer apellido"
           {...register("flastname", {
             required: true,
+            pattern: {
+              value: /^[A-Za-z ]+$/,
+              message: "Ingrese solo letras"
+            }
           })}
         />
+        {errors.flastname?.type === 'pattern' && <p className="messages">Ingrese solo letras</p>}
       </Form.Group>
       <Form.Group>
         <Form.Label>Segundo apellido</Form.Label>
         <Form.Control
           type="text"
-          placeholder="ingrese su segundo apellido"
-          {...register("slastname", { required: true })}
+          placeholder="Ingrese su segundo apellido"
+          {...register("slastname", { 
+            required: true,
+            pattern: {
+              value: /^[A-Za-z ]+$/,
+              message: "Ingrese solo letras"
+            }
+           })}
         />
+        {errors.slastname?.type === 'pattern' && <p className="messages">Ingrese solo letras</p>}
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Numero de identificacion</Form.Label>
         <Form.Control
           type="text"
-          placeholder="ingrese su identificacion"
-          {...register("identificacion", { required: true })}
+          placeholder="Ingrese su identificacion"
+          {...register("identificacion", { required: true, maxLength: 10 })}
         />
+        {errors.identificacion?.type === 'required' && <p className="messages" >Número de identificación es requerido</p>}
+        {errors.identificacion?.type === 'maxLength' && <p className="messages" >Número de máximo 10 dígitos</p>}
       </Form.Group>
       
       <Form.Group>
         <Form.Label>Correo</Form.Label>
         <Form.Control
           type="text"
-          placeholder="ingrese correo"
-          {...register("correo", {})}
+          placeholder="Ingrese correo"
+          {...register("correo", {
+            pattern: {
+              value: /[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}/,
+              message: "Correo inválido"
+            }
+
+          })}
         />
+        {errors.correo?.type === 'pattern' && <p className="messages">Correo inválido</p>}
       </Form.Group>
       <Form.Group>
         <Form.Label>Usuario</Form.Label>
         <Form.Control
           type="text"
           placeholder="ingrese usuario"
-          {...register("usuario", {})}
+          {...register("usuario", {
+            pattern: {
+              value: /^[A-Za-z0-9_]+$/,
+              message: "Ingrese solo letras"
+            }
+          })}
         />
+        {errors.usuario?.type === 'pattern' && <p className="messages">Ingrese solo letras, números y guiones bajos</p>}
       </Form.Group>
       <Form.Group>
         <Form.Label>Contraseña</Form.Label>
         <Form.Control
           type="password"
-          placeholder="ingrese contraseña"
-          {...register("contrasena", {})}
+          placeholder="Ingrese contraseña"
+          {...register("contrasena", {
+            pattern: {
+              value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[\W_]).{8,}$/,
+              message: "Debe contener más de 8 caracteres, almenos 1 minuscula, mayuscula, número y caracter especial"
+            }
+          })}
         />
+        {errors.contrasena?.type === "pattern" && <p className="messages">Debe contener más de 8 caracteres, almenos 1 minuscula, mayuscula, número y caracter especial</p>}
       </Form.Group>
 
       <Button type="submit" className="boton">
